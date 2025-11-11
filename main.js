cd4654481f808da2d70269a73438a06e18545d7c
kepinn <salamwalaikum54@gmail.com.com>
2025-11-11 10:40:33 +0700

    update

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-import { log } from './logger'
-
-log('Import works!')
+function CekNilai() {
+  // Mengambil value dari elemen input nilai
+  // Simpan ke dalam vartabel bernama "Nilai"
+  let Nilai = Number(document.getElementById("Nilai").value)
+  
+  // menginisiasi variabel "hasil" isinya string kosong""
+  let hasil = ""
+  
+  // menentukan angka A B C D berdasarkan isi variabel "Nilai"
+  
+  // menggunakan percabangan if (jika)
+  
+  // dan membandingkan variabel "Nilai"
+  if (Nilai >= 88) {
+    hasil = "Nilai A (Sangat Baik) "
+  } else if (Nilai >= 75) {
+    hasil = "Nilai B (Baik) "
+  } else if (Nilai >= 51) {
+    hasil = "Nilai C (Cukup Baik) "
+  } else if (nilai >= 40) {
+    hasil = (Nilai)
+  }
+  
+  // tampilkan isi Vartebel "hasil ke elemen " hasil" di html
+  document.getElementById('hasil').innerText = hasil
+  
+}
